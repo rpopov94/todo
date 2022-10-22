573ce7b06f257ada67a26f357b434b1da9d81e17
Роман Попов <rpopov@web-bee.ru>
2022-10-22 20:40:23 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import './App.css';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return <Button variant='contained'>Hello World</Button>;
 }
 
 export default App;
