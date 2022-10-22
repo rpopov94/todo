0c114477e80b30aec7ec31fcf86f032f7e68356e
Роман Попов <rpopov@web-bee.ru>
2022-10-22 20:34:09 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default App;
